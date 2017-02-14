a29ff50e999f432d5accaf02a7451325e91844d1
ming1144 <https://github.com/ming1144>
2017-02-14 16:52:57 +0800

    20170214 Mac

--- main.cpp
+++ main.cpp
@@ -14,7 +14,6 @@
 #endif
 
 #define CPU_ONLY
-#define OUTSIDE_TEST
 
 #include <caffe\caffe.hpp>
 #include "register.h"
@@ -31,7 +30,6 @@ using std::string;
 
 /* Pair (label, confidence) representing a prediction. */
 typedef std::pair<string, float> Prediction;
-
 
 class Classifier {
 public:
@@ -50,10 +48,8 @@ private:
 	std::vector<float> Predict(const QImage& img);
 
 	void WrapInputLayer(std::vector<cv::Mat>* input_channels);
-	void WrapInputLayer(std::vector<QImage> input_channels);
 
 	void Preprocess(const cv::Mat& img , std::vector<cv::Mat>* input_channels);
-	void Preprocess(const QImage& img, std::vector<QImage> input_channels);
 
 private:
 	shared_ptr<Net<float> > net_;
@@ -110,7 +106,6 @@ Classifier::Classifier(const string& model_file,
 		<< "Number of labels is different from the output layer dimension.";
 }
 
-
 static bool PairCompare(const std::pair<float, int>& lhs,
 	const std::pair<float, int>& rhs) {
 	return lhs.first > rhs.first;
@@ -130,6 +125,20 @@ static std::vector<int> Argmax(const std::vector<float>& v, int N) {
 
 /* Return the top N predictions. */
 std::vector<Prediction> Classifier::Classify(const cv::Mat& img, int N) {
+	std::vector<float> output = Predict(img);
+
+	N = std::min<int>(labels_.size(), N);
+	std::vector<int> maxN = Argmax(output, N);
+	std::vector<Prediction> predictions;
+	for (int i = 0; i < N; ++i) {
+		int idx = maxN[i];
+		predictions.push_back(std::make_pair(labels_[idx], output[idx]));
+	}
+
+	return predictions;
+}
+
+std::vector<Prediction> Classifier::Classify(const QImage& img, int N) {
 	std::vector<float> output = Predict(img);
 
 	N = std::min<int>(labels_.size(), N);
@@ -183,12 +192,12 @@ void Classifier::SetMean(const string& mean_file) {
 	mean__ = QImage(mean_blob.width(), mean_blob.height(), QImage::Format_RGB32);
 	if (num_channels_ == 3)
 	{
-		QColor temp(meandata[0], meandata[1], meandata[2]);
+		QRgb temp = qRgb(meandata[2], meandata[1], meandata[0]);
 		mean__.fill(temp);
 	}
 	else
 	{
-		QColor temp(meandata[0], meandata[0], meandata[0]);
+		QRgb temp = qRgb(meandata[0], meandata[0], meandata[0]);
 		mean__.fill(temp);
 	}
 	
@@ -201,11 +210,7 @@ void Classifier::SetMean(const string& mean_file) {
 	* filled with this value. */
 	cv::Scalar channel_mean = cv::mean(mean);
 	mean_ = cv::Mat(input_geometry_, mean.type(), channel_mean);
-	cv::imwrite("meanfileCV.png", mean_);
-	mean__.save("meanfileQT.png");
 }
-
-
 
 std::vector<float> Classifier::Predict(const cv::Mat& img) {
 	Blob<float>* input_layer = net_->input_blobs()[0];
@@ -228,6 +233,35 @@ std::vector<float> Classifier::Predict(const cv::Mat& img) {
 	return std::vector<float>(begin, end);
 }
 
+std::vector<float> Classifier::Predict(const QImage& img) {
+	Blob<float>* input_layer = net_->input_blobs()[0];
+	input_layer->Reshape(1, num_channels_,
+		input_geometry_.height, input_geometry_.width);
+	/* Forward dimension change to all layers. */
+	net_->Reshape();
+
+	float* input_data = input_layer->mutable_cpu_data();
+	float* data = (float*)img.bits();
+
+	for (int y = 0; y < input_height_; y++)
+	{
+		for (int x = 0; x < input_width_; x++)
+		{
+			*input_data = *data;
+			input_data++;
+			data++;
+		}
+	}
+
+	net_->Forward();
+
+	/* Copy the output layer to a std::vector */
+	Blob<float>* output_layer = net_->output_blobs()[0];
+	const float* begin = output_layer->cpu_data();
+	const float* end = begin + output_layer->channels();
+	return std::vector<float>(begin, end);
+}
+
 /* Wrap the input layer of the network in separate cv::Mat objects
 * (one per channel). This way we save one memcpy operation and we
 * don't need to rely on cudaMemcpy2D. The last preprocessing
@@ -235,7 +269,6 @@ std::vector<float> Classifier::Predict(const cv::Mat& img) {
 * layer. */
 void Classifier::WrapInputLayer(std::vector<cv::Mat>* input_channels) {
 	Blob<float>* input_layer = net_->input_blobs()[0];
-
 	int width = input_layer->width();
 	int height = input_layer->height();
 	float* input_data = input_layer->mutable_cpu_data();
@@ -289,13 +322,15 @@ void Classifier::Preprocess(const cv::Mat& img,
 int startImg = 1;
 int endImg = 22;
 int maskHeight = 47;
+bool outsideTest = false;
+bool useQT = true;
 
 int main()
 {
 	int i, j, m;
 
 	string modelType = std::to_string(maskHeight);
-	modelType += "_SubMean_padding";
+	modelType += "_SubMean";
 
 	string trained_filename = "model_" + modelType + "/deploy.prototxt";
 	string mean_filename = "model_" + modelType + "/mean.binaryproto";
@@ -303,100 +338,144 @@ int main()
 	string label_filename = "model_" + modelType + "/labels.txt";
 	Classifier classifier(trained_filename, model_filename, mean_filename, label_filename);
 
-#ifndef OUTSIDE_TEST
-	_mkdir(modelType.c_str());
-	for (m = startImg; m <= endImg; m++)
+	if (!outsideTest)
 	{
-		string testImage_filename = "image/test";
-		testImage_filename += std::to_string(m);
-		testImage_filename += ".bmp";
-		
-		Mat testImg = cv::imread(testImage_filename);
-		Mat testImgGray(testImg);
-		cvtColor(testImg, testImgGray, CV_BGR2GRAY);
-
-		Mat result(testImg.rows, testImg.cols, testImg.type(), Scalar(0));
-		//Mat result = cv::imread(testImage_filename);
-
-		for (i = maskHeight/2; i < testImgGray.rows - maskHeight/2; i++)
+		_mkdir(modelType.c_str());
+		for (m = startImg; m <= endImg; m++)
 		{
-			for (j = maskHeight/2; j < testImgGray.cols - maskHeight/2; j++)
+			string testImage_filename = "image/test";
+			testImage_filename += std::to_string(m);
+			testImage_filename += ".bmp";
+
+			if (useQT)
 			{
-				Point centerPoint;
-				centerPoint.x = j;
-				centerPoint.y = i;
-				Rect Region(Point(j - maskHeight / 2, i - maskHeight / 2), Point(j + maskHeight / 2 + 1, i + maskHeight / 2 + 1));
-				Mat ROI(testImgGray, Region);
+				Mat testImg = cv::imread(testImage_filename);
+				Mat testImgGray(testImg);
+				cv::cvtColor(testImg, testImgGray, CV_BGR2GRAY);
 
-				std::vector<Prediction> predictions = classifier.Classify(ROI);
+				Mat result(testImg.rows, testImg.cols, testImg.type(), Scalar(0));
+				//Mat result = cv::imread(testImage_filename);
 
-				Prediction temp = predictions[0];
-				if (temp.first == "1")
+				for (i = maskHeight / 2; i < testImgGray.rows - maskHeight / 2; i++)
 				{
-					result.at<Vec3b>(centerPoint) = Vec3b::all(255);
+					for (j = maskHeight / 2; j < testImgGray.cols - maskHeight / 2; j++)
+					{
+						cv::Point centerPoint;
+						centerPoint.x = j;
+						centerPoint.y = i;
+						cv::Rect Region(Point(j - maskHeight / 2, i - maskHeight / 2), Point(j + maskHeight / 2 + 1, i + maskHeight / 2 + 1));
+						Mat ROI(testImgGray, Region);
+
+						std::vector<Prediction> predictions = classifier.Classify(ROI);
+
+						Prediction temp = predictions[0];
+						if (temp.first == "1")
+						{
+							result.at<Vec3b>(centerPoint) = Vec3b::all(255);
+						}
+					}
 				}
+
+				_chdir(modelType.c_str());
+				string resultFilename = "result_";
+				resultFilename += std::to_string(m);
+				resultFilename += "_";
+				resultFilename += modelType;
+				resultFilename += ".PNG";
+				imwrite(resultFilename, result);
+				_chdir("..");
+			}
+			else
+			{
+				QImage testImg(QString::fromStdString(testImage_filename));
+				QImage testImgGray(testImg);
+				testImgGray.convertToFormat(QImage::Format_Grayscale8);
+
+				QImage result(testImg.width(), testImg.height(), QImage::Format_Grayscale8);
+				
+				QRgb temp = qRgb(0, 0, 0);
+				result.fill(temp);
+				for (i = maskHeight / 2; i < testImgGray.height() - maskHeight / 2; i++)
+				{
+					for (j = maskHeight / 2; j < testImgGray.width() - maskHeight / 2; j++)
+					{
+						QRect rect(QPoint(j - maskHeight / 2, i - maskHeight / 2), QPoint(j + maskHeight / 2, i + maskHeight / 2));
+						QImage ROI = testImgGray.copy(rect);
+
+						std::vector<Prediction> predictions = classifier.Classify(ROI);
+
+						Prediction temp = predictions[0];
+						QRgb tempColor = qRgb(255, 255, 255);
+						if (temp.first == "1")
+						{
+							result.setPixel(j, i, tempColor);
+						}
+					}
+				}
+				_chdir(modelType.c_str());
+				string resultFilename = "result_";
+				resultFilename += std::to_string(m);
+				resultFilename += "_";
+				resultFilename += modelType;
+				resultFilename += ".PNG";
+				result.save(QString::fromStdString(resultFilename));
+				_chdir("..");
+
 			}
 		}
-		_chdir(modelType.c_str());
-		string resultFilename = "result_";
-		resultFilename += std::to_string(m);
-		resultFilename += "_";
-		resultFilename += modelType;
-		resultFilename += ".PNG";
-		imwrite(resultFilename, result);
-		_chdir("..");
 	}
-#else
-	_chdir("outsideTestImage");
-	string testImage_filename = "testImage.txt";
-	std::vector<string> testImages;
-	std::ifstream input(testImage_filename);
-
-	char temp[100];
-
-	while (input.getline(temp, sizeof(temp)))
+	else
 	{
-		string tempString(temp);
-		testImages.push_back(tempString);
-	}
-	
-	for (int m = 0; m < testImages.size(); m++)
-	{
-		Mat testImg = cv::imread(testImages[m]);
-		Mat testImgGray(testImg);
-		cvtColor(testImg, testImgGray, CV_BGR2GRAY);
+		_chdir("outsideTestImage");
+		string testImage_filename = "testImage.txt";
+		std::vector<string> testImages;
+		std::ifstream input(testImage_filename);
 
-		Mat result(testImg);
+		char temp[100];
 
-		for (i = maskHeight / 2; i < testImgGray.rows - maskHeight / 2; i++)
+		while (input.getline(temp, sizeof(temp)))
 		{
-			for (j = maskHeight / 2; j < testImgGray.cols - maskHeight / 2; j++)
-			{
-				Point centerPoint;
-				centerPoint.x = j;
-				centerPoint.y = i;
-				Rect Region(Point(j - maskHeight / 2, i - maskHeight / 2), Point(j + maskHeight / 2 + 1, i + maskHeight / 2 + 1));
-				Mat ROI(testImgGray, Region);
-
-				std::vector<Prediction> predictions = classifier.Classify(ROI);
-
-				Prediction temp = predictions[0];
-				if (temp.first == "1")
-				{
-					result.at<Vec3b>(centerPoint) = Vec3b::all(255);
-				}
-			}
+			string tempString(temp);
+			testImages.push_back(tempString);
 		}
 
-		string resultFilename = "result_";
-		resultFilename += modelType;
-		resultFilename += "_outsideTest-";
-		resultFilename += std::to_string(m);
-		resultFilename += ".PNG";
-		imwrite(resultFilename, result);
+		for (int m = 0; m < testImages.size(); m++)
+		{
+			Mat testImg = cv::imread(testImages[m]);
+			Mat testImgGray(testImg);
+			cvtColor(testImg, testImgGray, CV_BGR2GRAY);
+
+			Mat result(testImg);
+
+			for (i = maskHeight / 2; i < testImgGray.rows - maskHeight / 2; i++)
+			{
+				for (j = maskHeight / 2; j < testImgGray.cols - maskHeight / 2; j++)
+				{
+					Point centerPoint;
+					centerPoint.x = j;
+					centerPoint.y = i;
+					Rect Region(Point(j - maskHeight / 2, i - maskHeight / 2), Point(j + maskHeight / 2 + 1, i + maskHeight / 2 + 1));
+					Mat ROI(testImgGray, Region);
+
+					std::vector<Prediction> predictions = classifier.Classify(ROI);
+
+					Prediction temp = predictions[0];
+					if (temp.first == "1")
+					{
+						result.at<Vec3b>(centerPoint) = Vec3b::all(255);
+					}
+				}
+			}
+
+			string resultFilename = "result_";
+			resultFilename += modelType;
+			resultFilename += "_outsideTest-";
+			resultFilename += std::to_string(m);
+			resultFilename += ".PNG";
+			imwrite(resultFilename, result);
+		}
 	}
 
-#endif
 
 	return 0;
 }
